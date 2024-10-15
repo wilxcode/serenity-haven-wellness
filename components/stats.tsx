9ea895a53438502994b005f424f819e5571f9f77
Wilfried Prombo <wilfriedprombo02@gmail.com>
2024-10-15 23:47:03 +0100

    :lipstick: Section stats updated

--- components/stats.tsx
+++ components/stats.tsx
@@ -4,7 +4,7 @@ const Stats = () => {
       <div className="mx-auto w-full max-w-7xl px-4 md:px-6 lg:px-8 flex flex-col sm:flex-row md:flex-wrap items-center gap-y-16 justify-between *:flex *:flex-col xl:*:flex-row *:items-center *:gap-1 md:*:gap-2">
         <div>
           <span className="xl:text-5xl md:text-4xl text-2xl font-bold">
-            <span>1000+</span>
+            1,000+
           </span>
           <span className="xl:text-xl md:text-lg text-center xl:text-left">
             Lives <br /> Transformed
@@ -12,7 +12,7 @@ const Stats = () => {
         </div>
         <div>
           <span className="xl:text-5xl md:text-4xl text-2xl font-bold">
-            <span>95%</span>
+            95%
           </span>
           <span className="xl:text-xl md:text-lg text-center xl:text-left">
             Client <br /> Satisfaction
@@ -20,7 +20,7 @@ const Stats = () => {
         </div>
         <div>
           <span className="flex gap-1 xl:text-5xl md:text-4xl text-2xl font-bold">
-            <span>Top 10</span>
+            Top 10
           </span>
           <span className="xl:text-xl md:text-lg text-center xl:text-left">
             Mental <br /> Health Center
